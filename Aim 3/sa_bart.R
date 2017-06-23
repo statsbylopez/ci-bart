bcee27f5db5808cd69b353a6b313e7f03bba4ee5
liangyuanhu <lyuanh@gmail.com>
2017-06-23 11:56:27 -0400

    runs bart on data

--- Aim 3/sa_bart.R
+++ Aim 3/sa_bart.R
@@ -2,32 +2,32 @@
 rm(list=ls())
 source("config.R")
 library(BayesTree)
-igma = 1.0 #y = f(x) + sigma*z , z~N(0,1)
-n = 100 #number of observations
-set.seed(99)
-x=matrix(runif(n*10),n,10) #10 variables, only first 5 matter
-f= function(x){
-  5*(x[,1]*x[,2]) + 2*(x[,3]-.5)^2+2*x[,4]-5*x[,5]
-}
-
-Eys = f(x)
-prob<-exp(Eys)/(1+exp(Eys))
-prob
-
-runis <- runif(n,0,1)
-Y <- ifelse(runis < prob,1,0)
-bartFit = bart(x,Y,ndpost=200)
-
-plot(bartFit)
-
-fitmat = cbind(prob,bartFit$yhat.train.mean)
-colnames(fitmat) = c('y','Ey','lm','bart')
-print(cor(fitmat))
-
-#adjust binary offset = sensitivity analysis 
-a<-rep(c(qnorm(.2),qnorm(.5),qnorm(.3),qnorm(.4)),each=25)
-bartFit2 = bart(x,Y,ndpost=200,binaryOffset =a)
-plot(bartFit2)
+# igma = 1.0 #y = f(x) + sigma*z , z~N(0,1)
+# n = 100 #number of observations
+# set.seed(99)
+# x=matrix(runif(n*10),n,10) #10 variables, only first 5 matter
+# f= function(x){
+#   5*(x[,1]*x[,2]) + 2*(x[,3]-.5)^2+2*x[,4]-5*x[,5]
+# }
+# 
+# Eys = f(x)
+# prob<-exp(Eys)/(1+exp(Eys))
+# prob
+# 
+# runis <- runif(n,0,1)
+# Y <- ifelse(runis < prob,1,0)
+# bartFit = bart(x,Y,ndpost=200)
+# 
+# plot(bartFit)
+# 
+# fitmat = cbind(prob,bartFit$yhat.train.mean)
+# colnames(fitmat) = c('y','Ey','lm','bart')
+# print(cor(fitmat))
+# 
+# #adjust binary offset = sensitivity analysis 
+# a<-rep(c(qnorm(.2),qnorm(.5),qnorm(.3),qnorm(.4)),each=25)
+# bartFit2 = bart(x,Y,ndpost=200,binaryOffset =a)
+# plot(bartFit2)
 
 
 #--------------------------------------#
@@ -53,27 +53,69 @@ names(dat)<-c("age","race","spanish","insurance","income","education",
               "fupmonth","id","trx","days2trx","death","totdose","tstage")
 
 dat<- dat %>% mutate(dyearcat=ifelse(dyear %in% c(2004:2007), 1, ifelse(dyear %in% c(2008:2010),2,3)))
-dat$dyearcat2<- as.factor(dat$dyear)
+#dat$dyearcat2<- as.factor(dat$dyear)
+# 
+# which(sapply(dat,is.factor))
+# numindex<-which(sapply(dat,is.numeric)) #age and psa are continous
+# numindex
+# 
+# ind2<-numindex[c("race","spanish","insurance","income","education",
+#                  "deyo","gs","trx","tstage","dyearcat")]
+# 
+# facVars = lapply(dat[ , ind2],
+#                  function(x) {
+#                    x = as.factor(x)
+#                    x
+#                  })
+# 
+# dat1<-cbind(facVars, dat[ , - ind2])
+# 
+# which(sapply(dat1,is.factor))
 
-which(sapply(dat,is.factor))
-numindex<-which(sapply(dat,is.numeric)) #age and psa are continous
-numindex
+#proportion of death in each trx group 
+group_by(dat,trx) %>% summarise(n=n(), p = sum(death)/n)
 
-ind2<-numindex[c("race","spanish","insurance","income","education",
-                 "deyo","gs","trx","tstage","dyearcat")]
+#--------------------#
+#  Now  run BART # 
+set.seed(3847293)
+#covariates
+covs.cont=c("psa", "days2trx","age")
+covs.cat=c("race","spanish","insurance","income","education","deyo","gs","tstage","dyearcat")
+covs=c(covs.cont,covs.cat)
+ncovs=length(covs)
+#treatment
+treat=dat$trx
+#outcome
+y <- dat$death
 
-facVars = lapply(dat[ , ind2],
-                 function(x) {
-                   x = as.factor(x)
-                   x
-                 })
+#data for running bart on 
+data<- dat[,c("death","trx",covs)]
 
-dat1<-cbind(facVars, dat[ , - ind2])
+xt=as.matrix(data[,-1])
+xp1=as.matrix(data[data$trx==1,-1])
+xp2<- xp1
+xp3<- xp1
+xp2[,1]<- 2  #switch treatment label. switch 'surgery' to 'RT' 
+xp3[,1]<- 3  #switch 'surgery' to 'RT+implants'
 
-which(sapply(dat1,is.factor))
+bart_tot2 <- bart(x.train=xt,   y.train=y,  x.test=xp2)
+bart_tot3 <- bart(x.train=xt,   y.train=y,  x.test=xp3)
+
+#----------------------#
+# is the path correct? #
+save(bart_tot2, file = file.path(root, "bart_tot2.RData"))
+save(bart_tot3, file = file.path(root, "bart_tot3.RData"))
 
 
+#### results
+load(file.path(root, "bart_tot2.RData"))
+# first just effect of treatment on treated
+# differences in prob scale
+diffs=pnorm(bart_tot2$yhat.train[,data$trx==1])-pnorm(bart_tot2$yhat.test)
+mndiffs=apply(diffs,1,mean)
+mean(mndiffs) # -0.027 pts treated with surgery would have lower death rate
+sd(mndiffs) #.003
 
-
-
+# get a sense of t.e. heterogeneity
+hist(apply(diffs,2,mean))
 
